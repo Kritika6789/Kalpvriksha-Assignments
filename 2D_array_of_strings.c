5d198d3dd0f12da5c2b5f91da278521fe07c9711
Kritika6789 <kritikajain1805@gmail.com>
2025-01-07 20:59:21 +0530

    2d_array_of_string

--- 2D_array_of_strings.c
+++ 2D_array_of_strings.c
@@ -2,25 +2,38 @@
 #include<stdlib.h>
 #include<string.h>
 #include<limits.h>
+#include<stdbool.h>
 void solve(){
     int nrow,ncolumn;
-    printf("Enter number of rows:");
+   
+     printf("Enter number of rows:");
     scanf("%d",&nrow);
-    printf("Enter number of columns:");
+     printf("Enter number of columns:");
     scanf("%d",&ncolumn);
+    if(nrow>=1 && nrow<=10 && ncolumn>=1 && ncolumn<=101){
    char *name[nrow][ncolumn];
    for(int row=0;row<nrow;row++){
     for(int column=0;column<ncolumn;column++){
         name[row][column]=(char*)malloc(51*sizeof(char));
     }
    }
+   bool isValidString=true;
    for(int row=0;row<nrow;row++){
     for(int column=0;column<ncolumn;column++){
         printf("Name at (%d,%d):",row,column);
         scanf("%s",name[row][column]);
+        int length=strlen(name[row][column]);
+        if(length>50){
+           isValidString=false;
+            break;
+        }
     }
     printf("\n");
    }
+   if(!isValidString){
+     printf("string with a maximum length of 50 characters is allowed only");
+   }
+   else{
    int first_letter_vowel=0;
    int longest_name_length=INT_MIN;
    char *ans=" ";
@@ -30,9 +43,9 @@ void solve(){
          if(name[row][column][0]=='a' ||name[row][column][0]=='e'||name[row][column][0]=='i'||name[row][column][0]=='o'||name[row][column][0]=='u'||name[row][column][0]=='A'||name[row][column][0]=='E'||name[row][column][0]=='I'||name[row][column][0]=='O'||name[row][column][0]=='U'){
          first_letter_vowel++;
        }
-       int n=strlen(name[row][column]);
-       if(longest_name_length<n){
-        longest_name_length=n;
+       int length=strlen(name[row][column]);
+       if(longest_name_length<length){
+        longest_name_length=length;
         ans=name[row][column];
        }
         printf("%s ",name[row][column]);
@@ -41,9 +54,13 @@ void solve(){
    }
    printf("Number of names starting with a vowel:%d\n",first_letter_vowel);
    printf("Longest name : %s",ans);
-   
- 
-
+   }
+    }
+    else{
+        printf("Rule:\n");
+        printf("1.Number of row should be >=1 && <=10\n");
+        printf("2.Number of column should be >=1 && <=10\n");
+    }
 }
 int main(){
     solve();
