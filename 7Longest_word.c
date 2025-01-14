7b8da2f0fd5e730b7d0c3684bdc276a85e72c030
Kritika6789 <136303324+Kritika6789@users.noreply.github.com>
2025-01-14 19:12:24 +0530

    Update 7Longest_word.c

--- 7Longest_word.c
+++ 7Longest_word.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include<stdlib.h>
 #include<limits.h>
+#include<stdbool.h>
 int string_length(char *str){
     int index=0;
     while(str[index]!='\0'){
@@ -13,53 +14,57 @@ int max(int value1,int value2){
     return value1>value2?value1:value2;
 }
 void longest_word(char* str){
-    int length=string_length(str);
-    int ans=INT_MIN;
-    int count=0;
     int start=-1;
     int end=-1;
-    int ans_start=-1;
-    int ans_end=-1;
+    int length=string_length(str);
+    char *ans=(char*)malloc(100*sizeof(char));
+    int i=0;
+    int longest=INT_MIN;
+    int start_index=-1;
+    int end_index=-1;
+    bool isletter=false;
     for(int index=0;index<length;index++){
         if(str[index]!=' '){
             if(start==-1){
                 start=index;
+                isletter=true;
             }
-            count++;
         }
         else{
-            if(end==-1){
-                end=index-1;
+            if(isletter && end==-1){
+                end=index;
+                int length=end-start;
+                if(length>longest){
+                    longest=length;
+                    start_index=start;
+                    end_index=end;
+                    isletter=false;
+                }
             }
-            if(count>ans){
-                ans=count;
-                ans_start=start;
-                ans_end=end;
-            }
-            count=0;
             start=-1;
             end=-1;
         }
     }
-    if(start==-1){
-        printf("Empty string");
-        return;
-    }
-    if(end==-1){
+     if(isletter && end==-1){
                 end=length-1;
-    }
-     if(count>ans){
-                ans=count;
-                ans_start=start;
-                ans_end=end;
+                int length=end-start+1;
+                if(length>longest){
+                    longest=length;
+                    start_index=start;
+                    end_index=end;
+                }
             }
-    int length1=ans_end-ans_start+1;
-    char *result=(char*)malloc((length1+1)*sizeof(char));
-    int i=0;
-    for(int index=ans_start;index<=ans_end;index++){
-       result[i++]=str[index];
+            if(start==-1 && end==-1){
+                printf("Empty string");
+            }
+            else{
+                  int index=0;
+    for(int st=start_index;st<=end_index;st++){
+        ans[index++]=str[st];
     }
-    printf("%s",result);
+    printf("%s",ans);
+    }
+
 }
 int main() {
     // Write C code here
